29701c8fa6d13bf2e525426c1e73dd5d11563ef8
Berin <berin358@outlook.fr>
2025-11-19 15:51:54 +0100

    wiki basic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    site: 'https://drustinbelmont.github.io',
+    site: 'https://berin-d.github.io',
     base: '/app',
     integrations: [
         starlight({
-            title: 'Ma Mini App',
+            title: 'App',
+
             sidebar: [
                 {
-                    label: 'Accueil',
-                    link: '/',
+                    label: 'App',
+                    collapsed: false,
+                    items: [
+                        { label: 'Accueil', link: '/' },
+                        { label: 'Pages', link: '/pages/about' },
+                    ],
                 },
                 {
-                    label: 'Deuxième page',
-                    link: '/about',
+                    label: 'AfterWiki',
+                    collapsed: false,
+                    items: [
+                        { label: 'Accueil', link: '/afterwiki/' },
+                        { label: 'Intro', link: '/afterwiki/intro' },
+                        { label: 'NetCode', link: '/afterwiki/netcode' },
+                        { label: 'Guide', link: '/afterwiki/guide' },
+                    ],
                 },
             ],
         }),
     ],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
