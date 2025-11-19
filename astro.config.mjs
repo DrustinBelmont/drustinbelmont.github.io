de60d8ab4283ad89772f197385e70fe241c35491
Berin <berin358@outlook.fr>
2025-11-19 14:27:04 +0100

    change folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  site: 'https://drustinbelmont.github.io',
-  base: '/',
+    site: 'https://drustinbelmont.github.io',
+    base: '/app',
+    integrations: [
+        starlight({
+            title: 'Ma Mini App',
+            sidebar: [
+                {
+                    label: 'Accueil',
+                    link: '/index',
+                },
+                {
+                    label: 'Deuxième page',
+                    link: '/about',
+                },
+            ],
+        }),
+    ],
 });
